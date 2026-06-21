546f0cca49c1e1736a0954367b2bb2cc44acd606
Lukáš Kaňka <lukaskabc@gmail.com>
2026-06-21 22:38:31 +0200

    setup project

---
+++ src/main/java/cz/lukaskabc/minecraft/mod/stellarview/adastra/compatibility/AdAstraStellarviewcompatibility.java
@@ -0,0 +1,14 @@
+package cz.lukaskabc.minecraft.mod.stellarview.adastra.compatibility;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(AdAstraStellarviewcompatibility.MODID)
+public class AdAstraStellarviewcompatibility {
+    public static final String MODID = "adastra_stellarview_compatibility";
+
+    public AdAstraStellarviewcompatibility(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
---
+++ src/main/java/cz/lukaskabc/minecraft/mod/stellarview/adastra/compatibility/AdAstraStellarviewcompatibilityClient.java
@@ -0,0 +1,12 @@
+package cz.lukaskabc.minecraft.mod.stellarview.adastra.compatibility;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(value = AdAstraStellarviewcompatibility.MODID, dist = Dist.CLIENT)
+public class AdAstraStellarviewcompatibilityClient {
+    public AdAstraStellarviewcompatibilityClient(ModContainer container) {
+    }
+}
